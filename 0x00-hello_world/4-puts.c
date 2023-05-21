69b2c01195d3e6281528e43c7c9f1adef7a80ec3
Ask4real <kusimosemiu@gmail.com>
2023-05-20 18:18:12 -0700

    alx

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 
+/**
+ * this is a prgram that prints using putts
+ * this is additional info
+ */
 int main(void)
 {
 	char str[] = "\"Programming is like building a multilingual puzzle";
