53483b3b015c53956e32c98a393c44877c70d1c6
Ask4real <kusimosemiu@gmail.com>
2023-06-05 19:37:00 -0700

    upgraded

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -11,12 +11,12 @@ int main(void)
 {
 	char a;
 
-	for (a ='0'; a <='9'; a++)
+	for (a = '0'; a <= '9'; a++)
 	{
 		putchar(a);
 	}
 
-	for (a ='a'; a <='f'; a++)
+	for (a = 'a'; a <= 'f'; a++)
 	{
 		putchar(a);
 	}
