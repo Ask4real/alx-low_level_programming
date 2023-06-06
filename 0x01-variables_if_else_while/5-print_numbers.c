dab7926e2944981be30530f9d75ed4301443f0fa
Ask4real <kusimosemiu@gmail.com>
2023-06-05 18:34:49 -0700

    updated

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main -  a program that prints all sing;e digit numbers of base 10
+ * starting from zero
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+
+{
+	int a;
+
+	printf("0123456789\n");
+
+	return (0);
+}
